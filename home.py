5f732306ebd14086700ad3c34b8e5096282ea522
KhaiThoMx <664230033@webmail.npru.ac.th>
2025-11-19 15:26:28 +0700

    Update home.py

--- home.py
+++ home.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
+# import plotly.express as px
+# import plotly.graph_objects as go
 
 st.header("Seksun")
 st.image("img/seksun.jpg")
